09eabcdef0ecc3a487d945ea208461c54d7f36cc
ShiNoragami <Dona.boy12@gmail.com>
2021-02-20 17:15:15 +0100

    redesign tablet layout, clean code

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/EstateManagerStream.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/EstateManagerStream.java
@@ -12,6 +12,7 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
+
 public class EstateManagerStream {
 
     /**
